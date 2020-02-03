fe4a4822834fdde5bd8fc2bed212ae6e111898ad
jairofilho79 <jairofilho79@gmail.com>
2020-02-02 22:13:22 -0300

    card sizing

--- src/app/contact/list-contact/card-contact/name.pipe.ts
+++ src/app/contact/list-contact/card-contact/name.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name:'name'})
 export class NamePipe implements PipeTransform {
   transform(value: string) {
-    if(value.length > 12) return value.substring(0,12)+'...'
+    if(value.length > 23) return value.substring(0,23)+'...'
     return value
   }
 
